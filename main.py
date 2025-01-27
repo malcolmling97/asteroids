c10211ae8ea2ded47b12a5dd1a8eec0aed946ccb
malcolmling97 <malcolmling97@gmail.com>
2025-01-27 21:48:47 +0800

    added bullets

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import pygame
 from constants import *
-from player import Player
+from player import Player,Shot
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 
@@ -14,11 +14,14 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     
     
     Asteroid.containers = (asteroids, updatable,drawable)
     AsteroidField.containers = (updatable)
     asteroidfield = AsteroidField()
+
+    Shot.containers = (shots,updatable,drawable)
 
     Player.containers = (updatable,drawable)
     player = Player( SCREEN_WIDTH / 2,  SCREEN_HEIGHT / 2)
--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS,PLAYER_TURN_SPEED,PLAYER_SPEED
+from constants import *
 
 class Player(CircleShape):
 
@@ -33,13 +33,28 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_w]:
             self.move(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self,dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
 
+
+class Shot(CircleShape):
+    def __init__(self,x,y):
+        super().__init__(x,y,radius=SHOT_RADIUS)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
 
 
 
