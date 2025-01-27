7c882bf671439810d6db7bf2e54e8438ed107e98
malcolmling97 <malcolmling97@gmail.com>
2025-01-27 21:00:25 +0800

    created groupings for objects

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ from player import Player
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()    
-    dt = 0
-    print("Starting asteroids!")
-    print(f"Screen width:{SCREEN_WIDTH}")
-    print(f"Screen height:{SCREEN_HEIGHT}")
-    player = Player( SCREEN_WIDTH / 2,  SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()    
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable,drawable)
+    player = Player( SCREEN_WIDTH / 2,  SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -20,8 +23,12 @@ def main():
         
         
         pygame.Surface.fill(screen, (0,0,0))
-        player.update(dt)
-        player.draw(screen)
+
+        for obj in updatable:
+            obj.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
 
